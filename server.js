3a15e6b78736138638a310fb5b8dcb99769b85ba
Elizabeth Woodard <eswoodard75@gmail.com>
2018-09-24 19:06:35 -0400

    add auth file

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+app.use(cors());
 
-app.get('/api/*', (req, res) => {
+const {
+  PORT, DATABASE_URL,
+} = require('./config');
+
+app.get('/flashdeck/*', (req, res) => {
   res.json({ok: true});
 });
 
