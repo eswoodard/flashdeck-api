2082c49c78f47c3634c8d7b9ca482c3a94d7cfe7
Elizabeth Woodard <eswoodard75@gmail.com>
2018-09-24 12:06:20 -0400

    update server.js

--- test/test-server.js
+++ test/test-server.js
@@ -1,12 +1,20 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app} = require('../server');
+const {app, runServer, closeServer} = require('../server');
 
 const should = chai.should();
 chai.use(chaiHttp);
 
 describe('API', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
 
   it('should 200 on GET requests', function() {
     return chai.request(app)
